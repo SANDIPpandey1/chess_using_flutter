4cdc048d5f364f05e351504120c32c6ff67e85df
Sohan <sosohan404@gmail.com>
2024-07-15 14:39:08 +0545

    Pieces Added

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -28,7 +28,7 @@ class _GameBoardState extends State<GameBoard> {
     List<List<ChessPiece?>> newBoard =
         List.generate(8, (index) => List.generate(8, (index) => null));
 
-    //pawn
+    // pawn
     for (int i = 0; i < 8; i++) {
       newBoard[1][i] = ChessPiece(
           type: ChessPieceType.pawn,
@@ -40,13 +40,80 @@ class _GameBoardState extends State<GameBoard> {
           imagePath: 'lib/assets/Chess_plt45.svg.png');
     }
 
-    //rooks
+    // rooks
+    newBoard[0][0] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: false,
+        imagePath: 'lib/assets/Chess_rlt45.svg.png');
+    newBoard[0][7] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: false,
+        imagePath: 'lib/assets/Chess_rlt45.svg.png');
+    newBoard[7][0] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: true,
+        imagePath: 'lib/assets/Chess_rlt45.svg.png');
+    newBoard[7][7] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: true,
+        imagePath: 'lib/assets/Chess_rlt45.svg.png');
 
-    //knights
+    // knights
+    newBoard[0][1] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: false,
+        imagePath: 'lib/assets/Chess_nlt45.svg.png');
+    newBoard[0][6] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: false,
+        imagePath: 'lib/assets/Chess_nlt45.svg.png');
+    newBoard[7][1] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: true,
+        imagePath: 'lib/assets/Chess_nlt45.svg.png');
+    newBoard[7][6] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: true,
+        imagePath: 'lib/assets/Chess_nlt45.svg.png');
 
-    //queen
+    // bishops
+    newBoard[0][2] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: false,
+        imagePath: 'lib/assets/Chess_blt45.svg.png');
+    newBoard[0][5] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: false,
+        imagePath: 'lib/assets/Chess_blt45.svg.png');
+    newBoard[7][2] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: true,
+        imagePath: 'lib/assets/Chess_blt45.svg.png');
+    newBoard[7][5] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: true,
+        imagePath: 'lib/assets/Chess_blt45.svg.png');
 
-    //king
+    // queens
+    newBoard[0][3] = ChessPiece(
+        type: ChessPieceType.queen,
+        isWhite: false,
+        imagePath: 'lib/assets/Chess_qlt45.svg.png');
+    newBoard[7][3] = ChessPiece(
+        type: ChessPieceType.queen,
+        isWhite: true,
+        imagePath: 'lib/assets/Chess_qlt45.svg.png');
+
+    // kings
+    newBoard[0][4] = ChessPiece(
+        type: ChessPieceType.king,
+        isWhite: false,
+        imagePath: 'lib/assets/Chess_klt45.svg.png');
+    newBoard[7][4] = ChessPiece(
+        type: ChessPieceType.king,
+        isWhite: true,
+        imagePath: 'lib/assets/Chess_klt45.svg.png');
+
     board = newBoard;
   }
 
